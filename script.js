074df215713ac7f9cae9cbb56093d9d01a8071b9
malindidev <thedealofficial22@gmail.com>
2024-11-06 10:46:52 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,6 @@
-// Smooth scrolling for links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
+// Copy Server Address Functionality
+document.querySelector(".copy-btn").addEventListener("click", () => {
+    const serverAddress = "PLAY.EYSERVER.COM";
+    navigator.clipboard.writeText(serverAddress);
+    alert("Server address copied to clipboard!");
 });
-
-// Scroll-to-Top Button
-const topBtn = document.getElementById("topBtn");
-
-// Show or hide the "Scroll to Top" button based on scroll position
-window.onscroll = function() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    topBtn.style.display = "block";
-  } else {
-    topBtn.style.display = "none";
-  }
-};
-
-// Scroll to the top of the page when the button is clicked
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
-}
