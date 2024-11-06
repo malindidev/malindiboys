ca7d324b69d132ed5322c8709996918ad0344d92
malindidev <thedealofficial22@gmail.com>
2024-11-06 10:23:45 +0000

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 // Scroll-to-Top Button
 const topBtn = document.getElementById("topBtn");
 
+// Show or hide the "Scroll to Top" button based on scroll position
 window.onscroll = function() {
   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
     topBtn.style.display = "block";
@@ -19,7 +20,8 @@ window.onscroll = function() {
   }
 };
 
+// Scroll to the top of the page when the button is clicked
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
 }
